d8c1a02d10204018ccdde8fcd860257a7726ab44
Rakshith Mohan <rakshith21mohan@gmail.com>
2023-12-29 15:52:42 +0530

    changed router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -11,9 +11,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App/>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
